377530a2320731389fe0fcd2fa6a56320f66de1a
Ed Robinson <edward-robinson@cookpad.com>
2018-06-06 14:14:54 +0100

    Inital go thingy

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/eks"
+	"text/template"
+)
+
+var sess = session.Must(session.NewSession())
+var kubeconfig = template.Must(template.New("kubeconfig").Parse(`
+apiVersion: v1
+kind: Config
+clusters:
+- name: {{.ClusterName}}
+  cluster:
+    server: {{.Endpoint}}
+    certificate-authority-data: {{.CertificateData}}
+contexts:
+- name: kubelet
+  context:
+    cluster: {{.ClusterName}}
+    user: kubelet
+current-context: kubelet
+users:
+- name: kubelet
+  user:
+    exec:
+      apiVersion: client.authentication.k8s.io/v1alpha1
+      command: /usr/local/bin/heptio-authenticator-aws
+      args:
+        - token
+        - "-i"
+        - "{{.ClusterName}}"
+`))
+
+func main() {
+	svc := eks.New(sess)
+	input := &eks.ListClustersInput{}
+
+	result, err := svc.ListClusters(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case eks.ErrCodeInvalidParameterException:
+				fmt.Println(eks.ErrCodeInvalidParameterException, aerr.Error())
+			case eks.ErrCodeClientException:
+				fmt.Println(eks.ErrCodeClientException, aerr.Error())
+			case eks.ErrCodeServerException:
+				fmt.Println(eks.ErrCodeServerException, aerr.Error())
+			case eks.ErrCodeServiceUnavailableException:
+				fmt.Println(eks.ErrCodeServiceUnavailableException, aerr.Error())
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+		return
+	}
+
+	fmt.Println(result)
+}
