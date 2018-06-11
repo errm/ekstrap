2a5ee4a3c1e2a558b95ca92719bf699ee3cd0fc9
Ed Robinson <edward-robinson@cookpad.com>
2018-06-11 16:23:38 +0100

    Refactor so we don't swallow template parsing errors

--- pkg/system/system.go
+++ pkg/system/system.go
@@ -11,51 +11,6 @@ import (
 	"text/template"
 )
 
-var kubeconfig = template.Must(template.New("kubeconfig").Parse(
-	`apiVersion: v1
-kind: Config
-clusters:
-- name: {{.Cluster.Name}}
-  cluster:
-    server: {{.Cluster.Endpoint}}
-    certificate-authority-data: {{.Cluster.CertificateAuthority.Data}}
-contexts:
-- name: kubelet
-  context:
-    cluster: {{.Cluster.Name}}
-    user: kubelet
-current-context: kubelet
-users:
-- name: kubelet
-  user:
-    exec:
-      apiVersion: client.authentication.k8s.io/v1alpha1
-      command: /usr/local/bin/heptio-authenticator-aws
-      args:
-        - token
-        - "-i"
-        - "{{.Cluster.Name}}"
-`))
-
-var kubeletService = template.Must(template.New("kubelet-service").Parse(
-	`[Unit]
-Description=kubelet: The Kubernetes Node Agent
-Documentation=http://kubernetes.io/docs/
-After=docker.service
-Requires=docker.service
-
-[Service]
-ExecStart=/usr/bin/kubelet   --address=0.0.0.0   --allow-privileged=true   --cloud-provider=aws   --cluster-dns=10.100.0.10   --cluster-domain=cluster.local   --cni-bin-dir=/opt/cni/bin   --cni-conf-dir=/etc/cni/net.d   --container-runtime=docker   --node-ip={{.Node.PrivateIpAddress}}   --network-plugin=cni   --cgroup-driver=cgroupfs   --register-node=true   --kubeconfig=/var/lib/kubelet/kubeconfig   --feature-gates=RotateKubeletServerCertificate=true   --anonymous-auth=false   --client-ca-file=/etc/kubernetes/pki/ca.crt
-
-Restart=on-failure
-Restart=always
-StartLimitInterval=0
-RestartSec=10
-
-[Install]
-WantedBy=multi-user.target
-`))
-
 type Files interface {
 	Sync(io.Reader, string, os.FileMode) error
 }
@@ -87,11 +42,21 @@ func (s System) Configure(n *node.Node, cluster *eks.Cluster) error {
 		Node:    n,
 	}
 
-	if err := s.writeConfig("/var/lib/kubelet/kubeconfig", kubeconfig, info); err != nil {
+	template, err := kubeconfig()
+	if err != nil {
 		return err
 	}
 
-	if err := s.writeConfig("/lib/systemd/system/kubelet.service", kubeletService, info); err != nil {
+	if err := s.writeConfig("/var/lib/kubelet/kubeconfig", template, info); err != nil {
+		return err
+	}
+
+	template, err = kubeletService()
+	if err != nil {
+		return err
+	}
+
+	if err := s.writeConfig("/lib/systemd/system/kubelet.service", template, info); err != nil {
 		return err
 	}
 
@@ -118,4 +83,53 @@ func (s System) writeConfig(path string, templ *template.Template, data interfac
 func (s System) writeCertificate(path string, data string) error {
 	decoder := base64.NewDecoder(base64.StdEncoding, strings.NewReader(data))
 	return s.Files.Sync(decoder, path, 0640)
+}
+
+func kubeconfig() (*template.Template, error) {
+	return template.New("kubeconfig").Parse(
+		`apiVersion: v1
+kind: Config
+clusters:
+- name: {{.Cluster.Name}}
+  cluster:
+    server: {{.Cluster.Endpoint}}
+    certificate-authority-data: {{.Cluster.CertificateAuthority.Data}}
+contexts:
+- name: kubelet
+  context:
+    cluster: {{.Cluster.Name}}
+    user: kubelet
+current-context: kubelet
+users:
+- name: kubelet
+  user:
+    exec:
+      apiVersion: client.authentication.k8s.io/v1alpha1
+      command: /usr/local/bin/heptio-authenticator-aws
+      args:
+        - token
+        - "-i"
+        - "{{.Cluster.Name}}"
+`)
+}
+
+func kubeletService() (*template.Template, error) {
+	return template.New("kubelet-service").Parse(
+		`[Unit]
+Description=kubelet: The Kubernetes Node Agent
+Documentation=http://kubernetes.io/docs/
+After=docker.service
+Requires=docker.service
+
+[Service]
+ExecStart=/usr/bin/kubelet   --address=0.0.0.0   --allow-privileged=true   --cloud-provider=aws   --cluster-dns=10.100.0.10   --cluster-domain=cluster.local   --cni-bin-dir=/opt/cni/bin   --cni-conf-dir=/etc/cni/net.d   --container-runtime=docker   --node-ip={{.Node.PrivateIpAddress}}   --network-plugin=cni   --cgroup-driver=cgroupfs   --register-node=true   --kubeconfig=/var/lib/kubelet/kubeconfig   --feature-gates=RotateKubeletServerCertificate=true   --anonymous-auth=false   --client-ca-file=/etc/kubernetes/pki/ca.crt
+
+Restart=on-failure
+Restart=always
+StartLimitInterval=0
+RestartSec=10
+
+[Install]
+WantedBy=multi-user.target
+`)
 }
