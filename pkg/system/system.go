2d0512ec0bed70dd212ac16875cce2383db7cc35
Ed Robinson <edward-robinson@cookpad.com>
2018-06-12 07:36:37 +0100

    Move config building code

--- pkg/system/system.go
+++ pkg/system/system.go
@@ -3,8 +3,12 @@ package system
 import (
 	"github.com/aws/aws-sdk-go/service/eks"
 	"github.com/errm/ekstrap/pkg/node"
+
+	"bytes"
+	"encoding/base64"
 	"io"
 	"os"
+	"text/template"
 )
 
 type Filesystem interface {
@@ -52,4 +56,43 @@ func (s System) Configure(n *node.Node, cluster *eks.Cluster) error {
 	}
 
 	return nil
+}
+
+func (s System) configs() ([]config, error) {
+	configs := []config{}
+	for path, content := range defaultTemplates {
+		template, err := template.New(path).Funcs(template.FuncMap{"b64dec": base64decode}).Parse(content)
+		if err != nil {
+			return configs, err
+		}
+		configs = append(configs, config{
+			template:   template,
+			path:       path,
+			filesystem: s.Filesystem,
+		})
+	}
+	return configs, nil
+}
+
+func base64decode(v string) (string, error) {
+	data, err := base64.StdEncoding.DecodeString(v)
+	if err != nil {
+		return "", err
+	}
+	return string(data), nil
+}
+
+type config struct {
+	template   *template.Template
+	path       string
+	filesystem Filesystem
+}
+
+func (c config) Write(data interface{}) error {
+	var buff bytes.Buffer
+	err := c.template.Execute(&buff, data)
+	if err != nil {
+		return err
+	}
+	return c.filesystem.Sync(&buff, c.path, 0640)
 }
